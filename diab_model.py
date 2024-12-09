0f2ee14909721f80c53fbf04b4e162ff8f54e7c5
Yakub Junaid <86046020+OLUYAKOBO@users.noreply.github.com>
2024-12-09 10:23:22 +0100

    changed the function name from predict to preprocess

--- diab_model.py
+++ diab_model.py
@@ -10,7 +10,7 @@ encoder = pickle.load(open ('encoder.pkl', "rb"))
 
 st.title('*Diabetes Mellitus Prediction*')
     
-def predict():
+def preprocess():
     c1,c2 = st.columns(2)
     with(c1):
         Age = st.number_input('*Please input your age*')
@@ -44,7 +44,7 @@ def predict():
         
         return df
         
-df = predict()
+df = preprocess()
 def preprocessing():
     
     df1 = df.copy()
